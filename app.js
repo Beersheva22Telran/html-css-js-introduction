6bb463290ecbff322d16e84409a36de08058bfad
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-07 15:48:51 +0300

    type conversions 1

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+//
+// let a = 5; //number
+// a = "hello"; //string
+// a = false; //boolean
+let a = 5 + "5";
+console.log(a, typeof a);
+a = a - 5;
+console.log(a, typeof a);
+a = "abc";
+a /= 2;
+console.log(a, typeof a);
