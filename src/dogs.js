341c4250f815bb1187d3404858acadef11911351
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-07 15:13:17 +0300

    intermediate commit

--- src/dogs.js
+++ src/dogs.js
@@ -1,12 +1,31 @@
 const detailsImageElement = document.querySelector(".details-image");
 const detailsTitleElement = document.querySelector(".details-title");
 const thumbnailsAnchors = document.querySelectorAll(".thumnails-ancor");
+const detailsSectionElement = document.querySelector(".details-section");
+const POINT_CLASS ="is-point";
+const mainSection = document.querySelector("main");
+const HIDDEN = "hidden";
 for (let i = 0; i < thumbnailsAnchors.length; i++) {
     thumbnailsAnchors[i].addEventListener("click", function() {
         setDetails(thumbnailsAnchors[i]);
     })
 }
+
 function setDetails(anchor) {
+    showDetails();
     detailsImageElement.src = anchor.getAttribute("data-details-image");
-    detailsTitleElement.innerHTML = anchor.getAttribute("data-details-text")
+    detailsTitleElement.innerHTML = anchor.getAttribute("data-details-text");
+    detailsSectionElement.classList.add(POINT_CLASS);
+    setTimeout(function() {
+        detailsSectionElement.classList.remove(POINT_CLASS);
+    }, 4000)
+}
+function showDetails() {
+    mainSection.classList.remove(HIDDEN);
+    
+    
+
+}
+function hideDetails () {
+    mainSection.classList.add(HIDDEN);
 }
