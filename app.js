d1da495fe6a4abb5a835f34b2880394e0513853d
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-16 15:57:48 +0300

    functions / this / bind / call /apply

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const x = {x:"x",toString: function() {return "x"}};
-const y = {y:"y",toString: function() {return "kuku"}};
-const d ={x:10, y: 20};
-x.toString = function() {return "xx"}
-d[x] = 100;
-d[y] = 200;
-const f = function() {};
-const num = 2;
-f.x = function(a, b) {
-    return a + b;
-}
-//console.log(f.x(10, 20));
+this.x = 100;
+this.width = 1000;
+this.height = 2000;
+//console.log(this);
+function f1() {
+    return this;
 
-//console.log(m.x);
-// [2].x = 10;
-// console.log([2].x);
-const ar = [2];
-ar.x = 10;
-// console.log(ar.x);
-// console.log(Array.from({length: 2}));
-// console.log(Array.from({length: 6}).map((__, index) => index + 5))
-// console.log(Array.from({length: 26})
-// .map((__, index) => String.fromCharCode(index + 'a'.charCodeAt(0)) )
-// .map(s => `<div>${s}</div>`).join(''));
-num.x = 100;
-console.log(num.x);
-const mm = {};
-mm.x = 100;
-console.log(mm.x);
-let ddd;
-//ddd.x = 200;
-(1 + 2).xxxx = 1000;
-let str1 = "abcd";
-str1[0] = "*";
-console.log(str1);
+}
+const f2 = () => {
+    return this;
+}
+// console.log('f1 call result', f1());
+// console.log('f2 call result', f2());
+// console.log((() => {
+//     console.log(this);
+// })());
+const x = {f1: function() {
+    return this;
+}, f2: () => {return this}};
+// console.log('x.f1 call result ', x.f1());
+// console.log('x.f2 call ', x.f2());
+const rectangle = {width: 20, height: 20, square: function() {
+    return this.width * this.height;
+}, perimeter: () => 2 * (this.width + this.height)};
+
+console.log("squre = " + rectangle.square());
+console.log("perimeter = " + rectangle.perimeter());
+const point = {x: 3, y: 4};
+function displayPoint(z, t) {
+    console.log(`x = ${this.x}, y = ${this.y}, z = ${z}, t = ${t} `)
+
+}
+ const displayPoint1 = displayPoint.bind(point, 100, 200);
+displayPoint.call(point, 200, 300);
+displayPoint.apply(point, [300, 400])
 
