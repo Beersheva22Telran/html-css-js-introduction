8dc7695a5731d7f0fb5585a516763031cc1f5f38
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-17 12:49:30 +0300

    Class / extends

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
-this.x = 100;
-this.width = 1000;
-this.height = 2000;
-//console.log(this);
-function f1() {
-    return this;
 
-}
-const f2 = () => {
-    return this;
-}
-// console.log('f1 call result', f1());
-// console.log('f2 call result', f2());
-// console.log((() => {
-//     console.log(this);
-// })());
-const x = {f1: function() {
-    return this;
-}, f2: () => {return this}};
-// console.log('x.f1 call result ', x.f1());
-// console.log('x.f2 call ', x.f2());
-const rectangle1 = {width: 20, height: 20, square: function() {
-    return this.width * this.height;
-}, perimeter: () => 2 * (this.width + this.height)};
-const rectangle2 = {width: 20, height: 20, square: function() {
-    return this.width * this.height;
-}, perimeter: () => 2 * (this.width + this.height)};
+class Rectangle {
+    #width;
+    #height;
+    constructor(width, height) {
+        this.#width = width;
+        this.#height = height;
 
-console.log("squre = " + rectangle1.square());
-console.log("perimeter = " + rectangle1.perimeter());
-const point = {x: 3, y: 4};
-function displayPoint(z, t) {
-    console.log(`x = ${this.x}, y = ${this.y}, z = ${z}, t = ${t} `)
+    }
+    square() {
 
+        return this.#width * this.#height;
+    }
+    perimeter() {
+        return 2 * (this.#width + this.#height);
+    }
 }
- const displayPoint1 = displayPoint.bind(point, 100, 200);
-// displayPoint.call(point, 200, 300);
-// displayPoint.apply(point, [300, 400]);
-console.log(rectangle1[0]);
-console.log(`rectangle1 == rectangle2 is ${rectangle1 == rectangle2}`)
-console.log(`JSON.stringify(rectangle1) == JSON.stringify(rectangle2) is ${JSON.stringify(rectangle1) == JSON.stringify(rectangle2)}`);
-const rectangle = JSON.parse(JSON.stringify(rectangle1));
-const rectangle3 = {...rectangle1}
+const rectangle = new Rectangle(3, 4);
+console.log(rectangle)
+let c;
+class Square extends Rectangle {
+    constructor(width) {
+        super(width, width);
+    }
+}
+const square = new Square(10);
+console.log(square.perimeter());
+Array.prototype.map = function() {
+    console.log(this)
+    return 'kuku'
+};
+const ar = [1, 2, 3, 4];
+console.log(ar.map());
 
