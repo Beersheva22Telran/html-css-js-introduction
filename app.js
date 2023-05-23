50685530f8975339923ce16d1f43503d41937720
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-23 12:23:38 +0300

    promise then async await

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function f3() {
 // const promise = sleep(2000);
 // promise.then(() => f1()).then(() => f2()).then(() => f3())
 function getId(predicate) {
-    const ids = [123, 124, 125];
+    const ids = [123, 124, 125, 126];
     const index = ids.findIndex(predicate);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -35,9 +35,29 @@ function getCar(id) {
      setTimeout(() => car ? resolve(car) : reject('no car found'), 1000))
 
 }
-function displayCar(predicate) {
-    getId(predicate).then(id => getCar(id)).then(car => console.log(car))
-    .catch(error => console.log(error));
+ function displayCar(predicate) {
+    return getId(predicate) 
+    .then(id => getCar(id))
+    
+    .then(car => console.log(car))
+    .catch(error => {
+        console.log(error);
+        //return 'mersedes'
+    })
+   
+    
 }
-displayCar(id => id == 125);
+async function displayCar(predicate) {
+    await sleep(20000)
+    try {
+        const id = await getId(predicate)
+    const car = await getCar(id);
+    console.log(car);
+    } catch (error) {
+       console.log(error) 
+    }
+    
+}
+displayCar(id => id == 126).then(() => console.log('thanks & good bye'));
+console.log('waiting for the data ...')
 
