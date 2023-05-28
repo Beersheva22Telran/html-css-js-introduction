d8d3f753fe9109af0e2ba66198df7ead6d3a7e6a
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-28 15:49:39 +0300

    adds join('') for setting options to a select HTML element

--- src/ui/WeatherForm.js
+++ src/ui/WeatherForm.js
@@ -104,5 +104,5 @@ export default class WeatherForm {
 }
 function setOptionItems(element, options, placeholder) {
     element.innerHTML = `<option value hidden selected>--${placeholder}--</option>`;
-    element.innerHTML += options.map(o => `<option value="${o}">${o}</option>`)
+    element.innerHTML += options.map(o => `<option value="${o}">${o}</option>`).join('')
 }
